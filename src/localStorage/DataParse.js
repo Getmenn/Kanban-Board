a18c121bcb11eb500913d118ece46bc354e631cf
Getmenn <112550048+Getmenn@users.noreply.github.com>
2022-11-16 19:58:20 +0500

    Delete DataParsed

--- src/localStorage/DataParse.js
+++
@@ -1,4 +0,0 @@
-export let dataBacklog = JSON.parse(localStorage.getItem('dataBacklog')) ;
-export let dataReady = JSON.parse(localStorage.getItem('dataReady')) ;
-export let dataInProgress = JSON.parse(localStorage.getItem('dataInProgress')) ;
-export let dataFinished = JSON.parse(localStorage.getItem('dataFinished')) ;  
